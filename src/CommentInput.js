a886478c77482203ecf7756410bfec14a067d9c6
gouzi <panchunxin@gouzi.local>
2020-02-24 22:31:35 +0800

    react-test

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -8,44 +8,66 @@ class CommentInput extends Component {
       content: ''
     }
   }
+  componentWillMount () {
+    this._loadUserName () 
+  }
   handleUserNameChange (event) {
-    this.setState({
+    this.setState ({
       userName: event.target.value
     })
   }
   handleContentChange (event) {
-    this.setState({
+    this.setState ({
       content: event.target.value
     })
   }
   handleSubmit () {
-    console.log(this.props)
     if (this.props.onSubmit) {
-      const {userName,content} = this.state
-      this.props.onSubmit((userName,content))
+      const { content,userName } = this.state
+      this.props.onSubmit({userName,content,createdTime: +new Date()})
+      this.setState ({
+        userName: '',
+        content: ''
+      })
     }
-    this.setState({
-      content: ''
-    })
+  }
+  handleUsernameBlur (event) {
+    this._saveUserName (event.target.value)
+  }
+  _saveUserName (value) {
+    localStorage.setItem('userName',value)
+    console.log(localStorage)
+  }
+  _loadUserName () {
+    if (localStorage.getItem('userName')) {
+      let userName = localStorage.getItem('userName')
+      this.setState ({
+        userName: userName
+      })
+    }
+  }
+  componentDidMount () {
+    this.textarea.focus()
   }
   render() {
     return (
       <div>
+        <div onClick={() => {this.props.child()}} ref='father'>触发父组件方法</div>
         <div className="comment-input">
           <div className="comment-field">
             <span className="comment-field-name">用户名：</span>
             <div className="comment-field-input">
-              <input onChange={this.handleUserNameChange.bind(this)} value={this.state.userName}/>
+              <input onBlur={this.handleUsernameBlur.bind(this)} onChange={this.handleUserNameChange.bind(this)} value={this.state.userName} />
             </div>
           </div>
           <div className="comment-field">
             <span className="comment-field-name">评论内容：</span>
             <div className="comment-field-input">
-              <textarea onChange={this.handleContentChange.bind(this)} value = {this.state.content}/>
+              <textarea ref={(ref) => {this.textarea = ref}} onChange={this.handleContentChange.bind(this)} value = {this.state.content}/>
             </div>
           </div>
           <div className="comment-field-button">
-            <button onClick={this.handleSubmit}>发布</button>
+            <button onClick={this.handleSubmit.bind(this)}>发布</button>
           </div>
         </div>
       </div>
--- src/CommentList.js
+++ src/CommentList.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react'
 import { deflate } from 'zlib'
 
 class ComponentList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
   render () {
+    const content = this.props.content
     return (
-      <div>Component</div>
+      <div>
+        {content.map ((item,index) => {
+          return (
+            <div key={index}>{item.userName}: {item.content}</div>
+          )
+        }) }
+      </div>
     )
   }
 }
