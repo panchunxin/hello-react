bd3a2f23342dbe8ea8b9ac06d1bb7fe33f072cca
gouzi <panchunxin@gouzi.local>
2020-02-24 22:31:35 +0800

    react-test

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react'
 import CommentInput from './CommentInput'
 import CommentList from './CommentList'
+import { classDeclaration, validate } from '@babel/types'
 
 class CommentApp extends Component {
+  constructor () {
+    super()
+    this.state = {
+      content: []
+    }
+  }
+  componentWillMount () {
+    this._loadContent()
+  }
   handleCommitContent (content) {
     console.log(content)
+    this.state.content.push (content)
+    this.setState({
+      content: this.state.content
+    })
+    this._saveContent ()
   } 
+  _saveContent () {
+    localStorage.setItem('content',JSON.stringify(this.state.content))
+  }
+  _loadContent () {
+    let content = localStorage.getItem('content')
+    if (content) {
+      content = JSON.parse(content)
+      this.setState({content})
+    }
+  }
+  parent () {
+    console.log('触发父组件方法成功')
+  }
   render () {
     return (
       <div className='wrapper'>
-        <CommentInput onSubmit={this.handleCommitContent.bind(this)}/>
-        <CommentList/>
+        <CommentInput onSubmit={this.handleCommitContent.bind(this)}  child={this.parent}/>
+        <CommentList content={this.state.content}/>
       </div>
     )
   }
